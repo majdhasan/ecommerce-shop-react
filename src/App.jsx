fa16da8de379f2f54de6a402709ff6678483e584
Majd Hasan <majd.hasan@aperto.com>
2020-11-18 21:55:12 +0100

    react shop init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <h1>Hello to my shop</h1>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      {/* <Logo></Logo>
+      <NavMenu></NavMenu> */}
+      this is the navbar
+    </div>
+  )
+}
+
+export default Header
