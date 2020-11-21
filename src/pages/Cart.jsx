212d2abaa1eb51fb0f5f02ea0550342696b4225d
Majd Hasan <majd.hasan@aperto.com>
2020-11-21 13:12:57 +0100

    change empty cart handling

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button';
 import { CartContext } from '../contexts/cartContext'
 import { useHistory } from 'react-router-dom'
 import CartDetails from '../components/CartDetails';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
     button: {
@@ -14,13 +15,16 @@ const useStyles = makeStyles({
 
 
 export default function Cart() {
+    const { cart } = useContext(CartContext);
     const classes = useStyles();
     const history = useHistory();
+    console.log(cart);
 
     return (
         <div>
-            <CartDetails />
-            <Button onClick={() => { history.push('/checkout') }} className={classes.button} fullWidth variant="contained" color="primary"> Checkout</Button>
+            { cart.cartTotal !== 0 ? <CartDetails /> : <Typography className={classes.button} variant={"h5"}>Your Cart is Empty</Typography>}
+
+            { cart.cartTotal !== 0 && <Button onClick={() => { history.push('/checkout') }} className={classes.button} fullWidth variant="contained" color="primary"> Checkout</Button>}
         </div>
     );
 }
