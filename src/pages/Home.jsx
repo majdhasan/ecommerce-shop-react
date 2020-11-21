5cb65ea8f3bb1a460c00d75fd2cee6037e96c686
Majd Hasan <majd.hasan@aperto.com>
2020-11-21 13:12:35 +0100

    add carousel component to landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import { fetchProducts } from '../services/api'
 import ProductCard from '../components/ProductCard';
+import LandingPageCarousel from '../components/LandingPageCarousel'
+import Container from '@material-ui/core/Container';
+
 
 const useStyles = makeStyles(() => ({
     container: {
@@ -29,16 +32,20 @@ function Home() {
 
     const classes = useStyles();
     return (
-
-        <Grid container className={classes.container} spacing={3}>
-            {products.map(product => {
-                return (
-                    <Grid key={product.id} xs={6} md={4} item>
-                        <ProductCard product={product}></ProductCard>
-                    </Grid>
-                )
-            })}
-        </Grid>
+        <div>
+            <LandingPageCarousel products={products}></LandingPageCarousel>
+            <Container>
+                <Grid container className={classes.container} spacing={3}>
+                    {products.map(product => {
+                        return (
+                            <Grid key={product.id} xs={6} md={4} item>
+                                <ProductCard product={product}></ProductCard>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </Container>
+        </div>
 
     )
 }
